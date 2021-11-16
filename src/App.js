3bbd5755cdd007782e3d911b1839966554016ce3
ChandimaHerath <chandimaherath@hotmail.com>
2021-11-16 15:00:11 +0530

    navigation redirecting issues fixed

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import NotFound from './components/common/notFound';
 import Customers from './components/customers';
 import Movies from './components/movies';
 import Rentals from './components/rentals';
-import {Route} from 'react-router-dom'
+import {Route,Redirect,Switch} from 'react-router-dom'
 
 class App extends Component{
   render(){
     return (
       <main className="container">
+        <Switch>
       <Route path="/movies"  component={Movies}>  </Route>
       <Route path="/customers"  component={Customers}>  </Route>
       <Route path="/rentals"  component={Rentals}>  </Route>
       <Route path="/not-found"  component={NotFound}>  </Route>
+      <Redirect from="/" exact to="/movies"/>
+      <Redirect to="/not-found"/>
 
-
+      </Switch>
 
     </main>
     );
