6c9e7a7bd414e301af40f0c0a7c40f14383fde9c
ChandimaHerath <chandimaherath@hotmail.com>
2021-11-16 14:49:14 +0530

    routing works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { Component } from 'react';
 import './App.css';
+import NotFound from './components/common/notFound';
+import Customers from './components/customers';
 import Movies from './components/movies';
+import Rentals from './components/rentals';
+import {Route} from 'react-router-dom'
 
 class App extends Component{
   render(){
     return (
       <main className="container">
-      <Movies/>
+      <Route path="/movies"  component={Movies}>  </Route>
+      <Route path="/customers"  component={Customers}>  </Route>
+      <Route path="/rentals"  component={Rentals}>  </Route>
+      <Route path="/not-found"  component={NotFound}>  </Route>
+
+
+
     </main>
     );
   }
