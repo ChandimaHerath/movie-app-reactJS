b1bcb6d28b4798644e6e434a4117a8491479c911
ChandimaHerath <chandimaherath@hotmail.com>
2021-11-17 10:16:12 +0530

    validation added for login form - UI didn't set yet

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -3,34 +3,56 @@ import React, { Component } from 'react';
 class LoginForm  extends React.Component {
 
         state = {
-            account :{username:'', password:'' }
+            account :{username:'', password:'' },
+            errors :{}
         }
 
+        validate(){
+             const errors = {};
+             const {account} = this.state;
+             if(account.username.trim()==='')
+             errors.username = 'Username is required';
+             if(account.password.trim()=== '')
+             errors.password = 'Password is required';
+
+             return Object.keys(errors).length === 0 ? null : errors;
+        };
+
+
         handleSubmit= e => {
-            e.preventDefault();
+             e.preventDefault();
+             const errors = this.validate();
+             console.log(errors);
+             this.setState ({errors});
+             if(errors) return;
+
+             console.log('submitted');
+             
+
         }
 
         handleChange=e=>{
             const account = {...this.state.account};
-            account.username = e.currentTarget.value;
+            account[e.currentTarget.name] = e.currentTarget.value;
             this.setState({account});
 
         }
 
     render() { 
+        const{account} = this.state;
         return <div>
-                <form onClick={this.handleSubmit}>
+                <form >
                  <div className="form-group">
                 <label htmlFor='username'>User Name</label>
-                <input onChange={this.handleChange} value={this.state.account.username} id="username" type="text" className="form-control" />
+                <input autoFocus name="username" onChange={this.handleChange} value={account.username} id="username" type="text" className="form-control" />
                 </div> 
 
                 <div className="form-group">
                 <label htmlFor='password'>Password</label>
-                <input id="password" type="password" className="form-control" />
+                <input name="password" onChange={this.handleChange} value={account.password} id="password" type="password" className="form-control" />
                 </div> 
                 </form>
-                <button className="btn btn-primary">Save</button>
+                <button  onClick={this.handleSubmit} className="btn btn-primary">Save</button>
         </div>;
     }
 }
