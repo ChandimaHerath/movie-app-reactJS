cbd337be0da539b3011318fd748a93ab27916fef
ChandimaHerath <chandimaherath@hotmail.com>
2021-11-17 10:49:18 +0530

    validation functionized in login form

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 class LoginForm  extends React.Component {
 
         state = {
             account :{username:'', password:'' },
-            errors :{}
+            errors : {}
         }
 
         validate(){
@@ -23,7 +23,7 @@ class LoginForm  extends React.Component {
              e.preventDefault();
              const errors = this.validate();
              console.log(errors);
-             this.setState ({errors});
+             this.setState ({errors : errors || {}});
              if(errors) return;
 
              console.log('submitted');
@@ -45,11 +45,13 @@ class LoginForm  extends React.Component {
                  <div className="form-group">
                 <label htmlFor='username'>User Name</label>
                 <input autoFocus name="username" onChange={this.handleChange} value={account.username} id="username" type="text" className="form-control" />
+                { this.state.errors.username&&<div className="alert alert-danger"> {this.state.errors.username} </div>}
                 </div> 
 
                 <div className="form-group">
                 <label htmlFor='password'>Password</label>
                 <input name="password" onChange={this.handleChange} value={account.password} id="password" type="password" className="form-control" />
+                { this.state.errors.password&&<div className="alert alert-danger"> {this.state.errors.password} </div>}
                 </div> 
                 </form>
                 <button  onClick={this.handleSubmit} className="btn btn-primary">Save</button>
